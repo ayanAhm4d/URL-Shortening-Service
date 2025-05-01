e26bdeb3367e429ae3c4f43d80865f65483f04ee
Ayan Ahmad <www.ayan007ahmad@gmail.com>
2025-05-01 08:01:43 +0530

    completed

--- main.go
+++ main.go
@@ -1,35 +1,22 @@
 package main
 
 import (
-	"log"
-
-	"github.com/ayanAhm4d/URL-shortener/api/database"
-	"github.com/ayanAhm4d/URL-shortener/api/routes"
+	"github.com/ayanAhm4d/URL-shortener/config"
+	"github.com/ayanAhm4d/URL-shortener/handlers"
+	"github.com/ayanAhm4d/URL-shortener/middleware"
+	"github.com/ayanAhm4d/URL-shortener/redis"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
-func setupRoutes(router *gin.Engine, db *database.MySQLStore) {
-	router.GET("/:url", func(c *gin.Context) {
-		routes.ResolveURL(c, db)
-	})
-	router.POST("/api/v1", func(c *gin.Context) {
-		routes.ShortenURL(c, db)
-	})
-}
-
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	config.LoadEnv()
+	redis.InitRedis()
 
-	db, err := database.NewMySQLStore()
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
-	defer db.Close()
-	router := gin.Default()
-	setupRoutes(router, db)
+	r := gin.Default()
+	r.Use(middleware.RateLimiter())
 
-	log.Fatal(router.Run(":3000"))
+	r.POST("/shorten", handlers.ShortenURL)
+	r.GET("/:short", handlers.ResolveURL)
+
+	r.Run(":8080")
 }
